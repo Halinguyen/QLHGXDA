e2cad731afcd4efa3911cea7d77d48e4ddd666c8
linhtruc430@gmail.com <linhtruc430@gmail.com>
2021-03-08 11:54:03 +0700

    third commit

---
+++ QLHGXDA/Controllers/QLXeController.cs
@@ -0,0 +1,27 @@
+﻿using QLHGXDA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLHGXDA.Controllers
+{
+    public class QLXeController : Controller
+    {
+        // GET: QLXe
+        public ActionResult Index()
+        {
+            if (Session["IsLogin"].Equals(true))
+            {
+                TTXe ttxe = new TTXe();
+                List<TTXe> dsThongtinxe = new List<TTXe>();
+                dsThongtinxe = ttxe.GetTTXe();
+                ViewBag.dsXe = dsThongtinxe;
+                return View();
+            }
+            else
+                return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ QLHGXDA/Models/TTXe.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace QLHGXDA.Models
+{
+    public class TTXe : tbl_Xe
+    {
+        string mavexe;
+        string tenLoaixe;
+
+        public string sMasove { get => mavexe; set => mavexe = value; }
+        public string sTenloaixe { get => tenLoaixe; set => tenLoaixe = value; }
+        public TTXe() { }
+
+        public TTXe(string pK_iXeID, string fK_iLoaixeID, string sBiensoxe, string sAnhxe, string mavexe, string tenLoaixe)
+        {
+            PK_iXeID = Convert.ToInt64(pK_iXeID);
+            FK_iLoaixeID = Convert.ToByte(fK_iLoaixeID);
+            this.sBiensoxe = sBiensoxe;
+            this.sAnhxe = sAnhxe;
+            this.mavexe = mavexe;
+            this.tenLoaixe = tenLoaixe;
+        }
+
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+        SqlDataReader dar;
+        SqlCommand cmd;
+        public List<TTXe> GetTTXe()
+        {
+            List<TTXe> dsTTxe = new List<TTXe>();
+            cmd = new SqlCommand("sp_GetTTVexe", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+            dar = cmd.ExecuteReader();
+            if (dar.HasRows)
+            {
+                while (dar.Read())
+                {
+                    TTXe ttxe = new TTXe(
+                         dar["PK_iXeID"].ToString(),
+                      dar["FK_iLoaixeID"].ToString(),
+                      dar["sBiensoxe"].ToString(),
+                      dar["sAnhxe"].ToString(),
+                    dar["sMasove"].ToString(),
+                      dar["sTenloaive"].ToString());
+                    dsTTxe.Add(ttxe);
+                }
+            }
+            conn.Close();
+            return dsTTxe;
+        }
+    }
+}
